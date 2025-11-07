575e4d6a5626d472215964a87b922e14789909a1
Precious Onyenaucheya <precious.onyenaucheya@ons.gov.uk>
2025-11-07 16:28:36 +0000

    remove comment

--- application.py
+++ application.py
@@ -68,6 +68,5 @@ def example(component_name, filename):
 
 
 if __name__ == "__main__":
-    # debug_mode = os.environ.get("FLASK_DEBUG", "0") == "1"
     debug_mode = os.environ.get("LOCAL_DEV") == "1"
     app.run(debug=debug_mode)
